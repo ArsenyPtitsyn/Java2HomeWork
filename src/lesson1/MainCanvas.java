11b840facd421108c9bd2b3479cb57e9e1dff28d
senyabird <senyabird@yandex.ru>
2020-05-13 16:56:51 +0300

    Exercise1 complited

---
+++ src/lesson1/MainCanvas.java
@@ -0,0 +1,46 @@
+package lesson1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainCanvas extends JPanel {
+
+    MainWindow gameController;
+    long lastFrameTime;
+
+    MainCanvas (MainWindow gameController) {
+        this.gameController = gameController;
+        lastFrameTime = System.nanoTime();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastFrameTime) * 0.000000001f;
+        gameController.onDrawFrame(this, g, deltaTime);
+        lastFrameTime = currentTime;
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        repaint();
+    }
+
+    public int getLeft() {
+        return 0;
+    }
+
+    public int getRight() {
+        return getWidth() - 1;
+    }
+
+    public int getTop() {
+        return 0;
+    }
+
+    public int getBottom() {
+        return getHeight() - 1;
+    }
+}
---
+++ src/lesson1/MainWindow.java
@@ -0,0 +1,57 @@
+package lesson1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    private static final int POS_X = 320;
+    private static final int POS_Y = 115;
+    private static final int WINDOW_WIDTH = 600;
+    private static final int WINDOW_HEIGHT = 450;
+
+    Sprite sprites[] = new Sprite[10];
+
+    private MainWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        setResizable(false);
+        MainCanvas canvas = new MainCanvas(this);
+        initApplication();
+        add(canvas);
+        setTitle("Circles");
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindow();
+            }
+        });
+    }
+
+    private void initApplication() {
+        for(int i = 0; i < sprites.length; i++) {
+            sprites[i] = new Ball();
+        }
+    }
+
+    void onDrawFrame(MainCanvas canvas, Graphics g, float deltaTime) {
+        update(canvas, deltaTime);
+        render(canvas, g);
+    }
+
+    private void update(MainCanvas canvas, float deltaTime) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].update(canvas, deltaTime);
+        }
+    }
+
+    private void render(MainCanvas canvas, Graphics g) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].render(canvas, g);
+        }
+    }
+}
