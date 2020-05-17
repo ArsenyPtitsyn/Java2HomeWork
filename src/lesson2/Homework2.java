bcaa1d3a7b3815367b6acfb1ea4c95be0a673b26
senyabird <senyabird@yandex.ru>
2020-05-17 15:53:24 +0300

    Primitive for a specific string

--- src/lesson2/Homework2.java
+++ src/lesson2/Homework2.java
@@ -4,15 +4,32 @@ public class Homework2 {
 
     public static void main(String[] arg) {
         final String initString= "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
-        String[] arrStr = transformString(initString);
-        for (int i = 0; i < arrStr.length; i++)
-            System.out.print(arrStr[i] + " ");
+        String[][] resArr = transformString(initString);
+        for (int i = 0; i < resArr.length; i++) {
+            for (int j = 0; j < resArr[i].length; j++) {
+                System.out.print(resArr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    //Написать метод, на вход которого подаётся такая строка,
+    // Написать метод, на вход которого подаётся такая строка,
     // метод должен преобразовать строку в двумерный массив типа String[][];
-    private static String[] transformString(String str) {
-        String separrtor = " ";
-        return str.split(separrtor);
+    private static String[][] transformString(String str) {
+
+        String[][] resArray = new String[4][4];
+
+        String separrtor1 = "\n";
+        String separrtor2 = " ";
+
+        String[] arrayStr1 = str.split(separrtor1);
+        for (int i = 0; i < arrayStr1.length; i++) {
+            String[] arrayStr2 = arrayStr1[i].split(separrtor2);
+            for (int j = 0; j < arrayStr2.length; j++) {
+                resArray[i][j] = arrayStr2[j];
+            }
+        }
+
+        return resArray;
     }
 }
