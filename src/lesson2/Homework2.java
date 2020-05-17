d5867b26a86fdcd4c937348ffb7a55a5baac64eb
senyabird <senyabird@yandex.ru>
2020-05-17 14:49:37 +0300

    Created an array with a single separator

--- src/lesson2/Homework2.java
+++ src/lesson2/Homework2.java
@@ -4,7 +4,9 @@ public class Homework2 {
 
     public static void main(String[] arg) {
         final String initString= "10 3 1 2 2 3 2 2 5 6 7 1 300 3 1 0";
-        transformString(initString);
+        String[] arrStr = transformString(initString);
+        for (int i = 0; i < arrStr.length; i++)
+            System.out.println(arrStr[i]);
     }
 
     //Написать метод, на вход которого подаётся такая строка,
