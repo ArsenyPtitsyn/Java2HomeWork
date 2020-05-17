89faf4bfe14a1d2741db2acee35b5536b125e2e1
senyabird <senyabird@yandex.ru>
2020-05-17 14:56:53 +0300

    Created an array with a single separator

--- src/lesson2/Homework2.java
+++ src/lesson2/Homework2.java
@@ -3,10 +3,10 @@ package lesson2;
 public class Homework2 {
 
     public static void main(String[] arg) {
-        final String initString= "10 3 1 2 2 3 2 2 5 6 7 1 300 3 1 0";
+        final String initString= "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
         String[] arrStr = transformString(initString);
         for (int i = 0; i < arrStr.length; i++)
-            System.out.println(arrStr[i]);
+            System.out.print(arrStr[i] + " ");
     }
 
     //Написать метод, на вход которого подаётся такая строка,
