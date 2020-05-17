fd248f0577e697cc2639a9e76e08a68d5f3e5622
senyabird <senyabird@yandex.ru>
2020-05-17 12:23:29 +0300

    initString created

--- src/lesson2/Homework2.java
+++ src/lesson2/Homework2.java
@@ -1,4 +1,9 @@
 package lesson2;
 
 public class Homework2 {
+
+    public static void main(String[] arg) {
+        final String initString= "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
+        
+    }
 }
