25d91d5aaff204a2e09db51fcff55595e9dc0c71
senyabird <senyabird@yandex.ru>
2020-05-17 14:38:53 +0300

    some code

--- src/lesson2/Homework2.java
+++ src/lesson2/Homework2.java
@@ -3,7 +3,14 @@ package lesson2;
 public class Homework2 {
 
     public static void main(String[] arg) {
-        final String initString= "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
-        
+        final String initString= "10 3 1 2 2 3 2 2 5 6 7 1 300 3 1 0";
+        transformString(initString);
+    }
+
+    //Написать метод, на вход которого подаётся такая строка,
+    // метод должен преобразовать строку в двумерный массив типа String[][];
+    private static String[] transformString(String str) {
+        String separrtor = " ";
+        return str.split(separrtor);
     }
 }
