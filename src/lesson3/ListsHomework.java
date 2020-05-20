9a9e33882ebd81b32dd75e9c548501aa3b996194
senyabird <senyabird@yandex.ru>
2020-05-20 18:41:11 +0300

    ArrayList as a list

--- src/lesson3/ListsHomework.java
+++ src/lesson3/ListsHomework.java
@@ -1,32 +1,45 @@
 package lesson3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class ListsHomework {
 
-    private static final String[] words = {"MySQL", "supports", "atomic", "Data",
-    "Definition", "Language", "statements", "This", "feature", "is", "referred",
-    "to", "as", "atomic", "DDL", "An", "atomic", "DDL", "statement", "combines",
-    "the", "data", "dictionary", "updates", "storage", "engine", "operations",
-    "and", "binary", "log", "writes", "associated", "with", "a", "DDL",
-    "operation", "into", "a", "single", "atomic", "operation"};
+    private static final String[] words = {"MySQL", "MySQL", "supports", "atomic", "Data",
+            "Definition", "Language", "statements", "This", "feature", "is", "supports",
+            "feature", "MySQL", "Language", "This", "Definition", "atomic", "statements",
+            "atomic", "Language", "MySQL", "This", "feature", "is", "Definition", "atomic",
+            "feature", "This", "supports", "MySQL"};
 
     public static void main(String[] args) {
         System.out.println(findListOfWords(words));
     }
 
-    // List of words in phrase without repetitions. Used HashSet to "increase" performance.
-    private static HashSet<String> findListOfWords(String[] s) {
-        HashSet<String> setOfWords = new HashSet<>();
-        for (int i = 0; i < s.length; i++) {
-            setOfWords.add(s[i]);
+    // List of words in phrase without repetitions.
+    private static ArrayList<String> findListOfWords(String[] s) {
+
+        ArrayList<String> listOfWords = new ArrayList<>();
+        listOfWords.add(s[0]);
+        for (int i = 1; i < s.length; i++) {
+            boolean mismatchCondition = !s[i].equals(s[0]);
+            for (int j = 0; j < i && mismatchCondition; j++) {
+                mismatchCondition = !s[i].equals(s[j]);
+            }
+            if (mismatchCondition)
+                listOfWords.add(s[i]);
         }
-        return setOfWords;
+        return listOfWords;
     }
 
     // Count how many times each word occurs.
-    private static HashMap<String, Integer> mapOfWords(String[] s) {
-        HashMap<String , Integer>
-    }
+//    private static HashMap<String, Integer> mapOfRepeatedWords(String[] s) {
+//
+//        int j = 0;
+//        for (int i = 0; i < s.length; i++) {
+//            if (s[i].equals(?))
+//                j++;
+//            else continue;
+//        }
+//    }
 }
