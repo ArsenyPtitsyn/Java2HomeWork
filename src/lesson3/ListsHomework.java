34e13f6437d739a1cd51935a8d3322677b0d7b8a
senyabird <senyabird@yandex.ru>
2020-05-20 14:05:34 +0300

    Created the array of strings

---
+++ src/lesson3/ListsHomework.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class ListsHomework {
+
+    private static final String[] words = {"MySQL", "supports", "atomic", "Data",
+    "Definition", "Language", "statements", "This", "feature", "is", "referred",
+    "to", "as", "atomic", "DDL", "An", "atomic", "DDL", "statement", "combines",
+    "the", "data", "dictionary", "updates", "storage", "engine", "operations",
+    "and", "binary", "log", "writes", "associated", "with", "a", "DDL",
+    "operation", "into", "a", "single", "atomic", "operation"};
+
+    
+}
