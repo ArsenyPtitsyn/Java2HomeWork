e5bd5beee2f041edddd28312d48f39a0d20abecb
senyabird <senyabird@yandex.ru>
2020-05-20 16:07:45 +0300

    Created list of words in phrase without repetitions.

--- src/lesson3/ListsHomework.java
+++ src/lesson3/ListsHomework.java
@@ -1,5 +1,8 @@
 package lesson3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class ListsHomework {
 
     private static final String[] words = {"MySQL", "supports", "atomic", "Data",
@@ -10,6 +13,20 @@ public class ListsHomework {
     "operation", "into", "a", "single", "atomic", "operation"};
 
     public static void main(String[] args) {
-        
+        System.out.println(findListOfWords(words));
+    }
+
+    // List of words in phrase without repetitions. Used HashSet to "increase" performance.
+    private static HashSet<String> findListOfWords(String[] s) {
+        HashSet<String> setOfWords = new HashSet<>();
+        for (int i = 0; i < s.length; i++) {
+            setOfWords.add(s[i]);
+        }
+        return setOfWords;
+    }
+
+    // Count how many times each word occurs.
+    private static HashMap<String, Integer> mapOfWords(String[] s) {
+        HashMap<String , Integer>
     }
 }
