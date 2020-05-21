e525a09d01f9303ac4c935d6ceb57d883892173a
senyabird <senyabird@yandex.ru>
2020-05-21 18:56:01 +0300

    Some adding

--- src/lesson3/PhoneBook.java
+++ src/lesson3/PhoneBook.java
@@ -1,4 +1,17 @@
 package lesson3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PhoneBook {
+
+
+
+    PhoneBook() {
+
+    }
+
+    private ArrayList<String> findNumberBySurname(String surName) {
+
+    }
 }
