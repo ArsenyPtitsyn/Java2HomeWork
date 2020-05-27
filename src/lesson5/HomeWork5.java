c788178238598639cdedd7ab77af677c3fba398e
senyabird <senyabird@yandex.ru>
2020-05-27 15:49:43 +0300

    ordinaryMethod generated

---
+++ src/lesson5/HomeWork5.java
@@ -0,0 +1,41 @@
+package lesson5;
+
+public class HomeWork5 {
+
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+    private static void ordinaryMethod() {
+
+        // Create a one-dimensional long array
+        float[] arr = new float[size];
+
+        // Fill the array with "1"
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        // Mark the time before starting to fill the array
+        long a = System.currentTimeMillis();
+
+        // Fill the array with new values using the formula
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) *
+                    Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        // Mark the time after filling the array
+        long b = System.currentTimeMillis();
+        System.out.println(b -a);
+    }
+
+    private static synchronized void multipleThreadsMethod() {
+
+        // Create a one-dimensional long array
+        float[] arr = new float[size];
+    }
+
+    public static void main(String[] args) {
+        ordinaryMethod();
+    }
+}
