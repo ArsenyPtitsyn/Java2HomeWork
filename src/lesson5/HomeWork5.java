12a3938704c0b5088d81f6cb81bafcce2f723d40
senyabird <senyabird@yandex.ru>
2020-05-27 23:00:17 +0300

    It works

--- src/lesson5/HomeWork5.java
+++ src/lesson5/HomeWork5.java
@@ -16,12 +16,12 @@ public class HomeWork5 {
         // Mark the time before starting to fill the array
         long a = System.currentTimeMillis();
         ordinaryMethod(arr);
-        System.out.println("Execution time: " + (System.currentTimeMillis() - a));
+        System.out.println("Execution time for ordinaryMethod: " + (System.currentTimeMillis() - a));
 
         fillArray(arr);
         a = System.currentTimeMillis();
-        multipleThreadsMethod();
-        System.out.println("Execution time: " + (System.currentTimeMillis() - a));
+        multiThreadsMethod(arr);
+        System.out.println("Execution time for multiThreadsMethod: " + (System.currentTimeMillis() - a));
     }
 
     private static void fillArray(float[] a) {
@@ -33,44 +33,21 @@ public class HomeWork5 {
     private static void ordinaryMethod(float[] a) {
 
         // Fill the array with new values using the formula
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < a.length; i++) {
             a[i] = (float) (a[i] * Math.sin(0.2f + i / 5) *
                     Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-
-        // Mark the time after filling the array
-        long b = System.currentTimeMillis();
-
     }
 
-    private static void multipleThreadsMethod() {
+    private static void fillHalfArray(float[] a, int half) {
 
-        // Create a one-dimensional long array and two split arrays
-        float[] arr = new float[size];
-
-        // Fill the array with "1"
-        for (int i = 0; i < size; i++) {
-            arr[i] = 1;
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (float) (a[i] * Math.sin(0.2f + (i + half) / 5) *
+                    Math.cos(0.2f + (i + half) / 5) * Math.cos(0.4f + (i + half) / 2));
         }
-
-        // Mark the time before starting to fill the array
-        long a = System.currentTimeMillis();
-
-        // Create two threads
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                fillArrays(arr);
-                long b = System.currentTimeMillis();
-                System.out.println(b - a);
-            }
-        };
-
-        new Thread(r, "Thread#1").start();
-        new Thread(r, "Thread#2").start();
     }
 
-    private static synchronized void fillArrays(float[] a) {
+    private static void multiThreadsMethod(float[] a) {
 
         // Split the array into two parts
         float[] a1 = new float[h];
@@ -78,18 +55,30 @@ public class HomeWork5 {
         System.arraycopy(a, 0, a1, 0, h);
         System.arraycopy(a, h, a2, 0, h);
 
-        // Fill the arrays with new values using the formula and two threads
-        for (int i = 0; i < h; i++) {
-            a1[i] = (float) (a[i] * Math.sin(0.2f + i / 5) *
-                    Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        // Create two threads and fill the halfArrays with values using formula
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HomeWork5.fillHalfArray(a1, 0);
+            }
+        });
+        t1.start();
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HomeWork5.fillHalfArray(a2, h);
+            }
+        });
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        for (int i = 0; i < h; i++) {
-            a2[i] = (float) (a[i + 500_000] * Math.sin(0.2f + (i + 500_000)/ 5) *
-                    Math.cos(0.2f + (i + 500_000)/ 5) * Math.cos(0.4f + (i + 500_000)/ 2));
-        }
 
-        // Merge arrays
+        // Merge halfArrays
         System.arraycopy(a1, 0, a, 0, h);
         System.arraycopy(a2, 0, a, h, h);
     }
