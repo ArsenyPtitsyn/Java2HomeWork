defc9ac6a28da08dcaca3e5620a267ede1252b7f
senyabird <senyabird@yandex.ru>
2020-06-03 22:39:00 +0300

    Added db

--- chat-server/src/ru/gb/chat/server/core/SqlClient.java
+++ chat-server/src/ru/gb/chat/server/core/SqlClient.java
@@ -1,4 +1,15 @@
 package ru.gb.chat.server.core;
 
 public class SqlClient {
+    synchronized static void connect() {
+
+    }
+
+    synchronized static void disconnect() {
+
+    }
+
+    synchronized static String getNickname(String login, String password) {
+        return null;
+    }
 }
